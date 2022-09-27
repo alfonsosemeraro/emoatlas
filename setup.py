dc9df5d15912b0645c0f383ad646d059fa50baaa
Finley Gibson <f.j.gibson@exeter.ac.uk>
2022-09-27 15:55:30 +0100

    added basic setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="emolib",
+    version="0.0.1",
+    description="installable version of emolib to improve usability",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7.00',
+)
