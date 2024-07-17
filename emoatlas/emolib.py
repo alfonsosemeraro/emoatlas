15ac9518b0093f80b6592d2a7ea63e7e434ab8fe
RiccardoImprota <riccardo.imp@gmail.com>
2024-07-17 18:02:00 +0200

    fixed drawformamentis

--- emoatlas/emolib.py
+++ emoatlas/emolib.py
@@ -373,6 +373,7 @@ class EmoScores:
                 alpha_hypernyms=alpha_hypernyms,
                 alpha_synonyms=alpha_synonyms,
                 save_path=save_path,
+                Valences=None,
             )
 
     def draw_formamentis_custom_valences(
