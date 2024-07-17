e37c5b998d212472accdf91427b9a482354031b7
RiccardoImprota <riccardo.imp@gmail.com>
2024-07-17 18:06:09 +0200

    valences fix

--- emoatlas/emolib.py
+++ emoatlas/emolib.py
@@ -373,7 +373,7 @@ class EmoScores:
                 alpha_hypernyms=alpha_hypernyms,
                 alpha_synonyms=alpha_synonyms,
                 save_path=save_path,
-                Valences=None,
+                valences=None,
             )
 
     def draw_formamentis_custom_valences(
